6afa6a8e1bcaa95e6e8dc81c715c48cfddb04c41
zhenghe <zhenghe@madadata.com>
2016-04-30 15:09:40 +0800

    add travis CI

---
+++ src/index.test.js
@@ -0,0 +1,28 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('starwars-names', function() {
+  describe('all', function() {
+
+    it('should be an array of strings', function() {
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'string';
+        });
+      };
+    });
+
+    it('should contain Han Solo', function() {
+      expect(starWars.all).to.include('Han Solo');
+    });
+  });
+
+  describe('random', function() {
+    it('should return a random item from starWars.all', function() {
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    })
+  });
+});
